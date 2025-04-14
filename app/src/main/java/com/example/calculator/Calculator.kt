3f07a721730279b1ec28c971246dc57192ba2115
Blagodatskih Serge <stikkas17@gmail.com>
2025-04-14 21:07:56 +0300

    first commit

---
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -0,0 +1,60 @@
+package com.example.calculator
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.calculator.ui.theme.LightGrey
+
+
+@Composable
+fun Calculator(
+    state: CalculatorState, modifier: Modifier = Modifier,
+    buttonSpacing: Dp = 8.dp
+) {
+    Box(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+            verticalArrangement = Arrangement.spacedBy(buttonSpacing)
+        ) {
+            Text(
+                state.number1 + (state.operation ?: "") + state.number2,
+                textAlign = TextAlign.End,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 32.dp),
+                fontWeight = FontWeight.Light,
+                fontSize = 80.sp,
+                color = Color.White,
+                maxLines = 2
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+            ) {
+                CalculatorButton(
+                    "AC", Modifier
+                        .background(LightGrey)
+                        .aspectRatio(2f)
+                        .weight(2f)
+                ) { }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.ui.unit.dp
+import com.example.calculator.ui.theme.CalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalculatorTheme {
+                val viewModel: CalculatorViewModel by viewModels()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(state, buttonSpacing = buttonSpacing)
+            }
+        }
+    }
+}
